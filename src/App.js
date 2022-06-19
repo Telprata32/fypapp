3c83421f40d1a6355f754dafc205dd0166d491d2
Abdul Rahim <zero_degree_96@hotmail.com>
2022-06-19 15:02:33 +0800

    Login page first edit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import shopimg from "./Images/Shopping_edited.jpg";
+import { Container, Row } from "react-bootstrap";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{ backgroundImage: `url(${shopimg})` }}>
+      <Row>
+        <h1>Hello</h1>
+      </Row>
+      <Row>
+        <h1>Try it now</h1>
+      </Row>
+    </Container>
   );
 }
 
