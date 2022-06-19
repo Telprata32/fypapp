fc78287d7d0e5e3465c28260125569dfae6e2988
Abdul Rahim <zero_degree_96@hotmail.com>
2022-06-19 16:30:40 +0800

    UI Checkpoint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import shopimg from "./Images/Shopping_edited.jpg";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Modal, Button, Col } from "react-bootstrap";
 import "./App.css";
 
 function App() {
@@ -9,7 +9,30 @@ function App() {
         <h1>Hello</h1>
       </Row>
       <Row>
-        <h1>Try it now</h1>
+        <Col
+          md="5"
+          style={{
+            backgroundColor: "white",
+          }}
+        >
+          <Modal.Dialog>
+            <Modal.Header closeButton>
+              <Modal.Title>Modal title</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+              <p>Modal body text goes here.</p>
+            </Modal.Body>
+
+            <Modal.Footer>
+              <Button variant="secondary">Close</Button>
+              <Button variant="primary">Save changes</Button>
+            </Modal.Footer>
+          </Modal.Dialog>
+        </Col>
+        <Col md="7">
+          <h1>2nd Column</h1>
+        </Col>
       </Row>
     </Container>
   );
