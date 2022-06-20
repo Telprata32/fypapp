9ac10c6e1966009ee6bfb310bcd6ea1122f3e39d
Abdul Rahim <zero_degree_96@hotmail.com>
2022-06-20 09:27:28 +0800

    Almost complete login UI ✅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import shopimg from "./Images/Shopping_edited.jpg";
-import { Container, Row, Modal, Button, Col } from "react-bootstrap";
+import logo from "./Images/App Logo.png";
+import {
+  Container,
+  Row,
+  Modal,
+  Button,
+  Col,
+  Form,
+  Image,
+  Stack,
+} from "react-bootstrap";
 import "./App.css";
 
 function App() {
@@ -21,20 +31,36 @@ function App() {
             borderRadius: "16px",
           }}
         >
-          <Modal.Dialog className="h-100">
-            <Modal.Header closeButton>
-              <Modal.Title>Modal title</Modal.Title>
-            </Modal.Header>
+          <div className="text-center">
+            <Image src={logo} />
+          </div>
+          <Form className="h-100 pt-5 px-2">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-            <Modal.Body>
-              <p>Modal body text goes here.</p>
-            </Modal.Body>
-
-            <Modal.Footer>
-              <Button variant="secondary">Close</Button>
-              <Button variant="primary">Save changes</Button>
-            </Modal.Footer>
-          </Modal.Dialog>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Stack gap="2" className="float-end" direction="horizontal">
+              <Button>Forgot Password</Button>
+              <Button
+                className="float-start ms-auto"
+                variant="primary"
+                type="submit"
+              >
+                Submit
+              </Button>
+            </Stack>
+          </Form>
         </Col>
       </Row>
     </Container>
