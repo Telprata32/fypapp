7d6f270423b8a98cfcad84b5ad6f3c61cd8faf7c
Abdul Rahim <zero_degree_96@hotmail.com>
2022-06-28 21:39:13 +0800

    Changes: > Fixed routing > Started on home page after logging in

--- src/App.js
+++ src/App.js
@@ -1,85 +1,18 @@
-import shopimg from "./Images/Shopping_edited.jpg";
-import logo from "./Images/appLogo.svg";
-import { Link } from "react-router-dom";
-import {
-  Container,
-  Row,
-  Modal,
-  Button,
-  Col,
-  Form,
-  Image,
-  Stack,
-} from "react-bootstrap";
-import "./App.css";
-
-// Import link to components for router
+import * as React from "react";
+import Login from "./Pages/Login";
+import Registration from "./Pages/Registration";
+import Home from "./Pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Container
-      fluid
-      className="vh-100"
-      style={{
-        backgroundImage: `url(${shopimg})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "100% 100%",
-        height: "600px",
-      }}
-    >
-      <Row className="h-100 py-4">
-        <Col
-          xs="4"
-          className="mx-auto pt-5 px-4 pb-4"
-          style={{
-            backgroundColor: "white",
-            borderRadius: "16px",
-          }}
-        >
-          <div className="text-center img-container">
-            <Image src={logo} />
-          </div>
-          <Form
-            style={{ display: "inlineBlock" }}
-            className="h-76 mt-2 pt-5 px-5"
-          >
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-
-            <Row>
-              If you don't have an account, click <Link>here</Link>
-            </Row>
-            <Stack
-              gap="2"
-              className="pt-4 mt-5 w-100 float-end align-bottom hstack gap-2"
-              direction="horizontal"
-            >
-              <Button className="float-start btn-danger">
-                Forgot Password
-              </Button>
-              <Button
-                className="ms-auto btn-color-orange"
-                variant="primary"
-                type="submit"
-                style={{ backgroundColor: "#FEB272" }}
-              >
-                Submit
-              </Button>
-            </Stack>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+    <div>
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,6 @@
 import shopimg from "../Images/Shopping_edited.jpg";
 import logo from "../Images/appLogo.svg";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {
   Container,
   Row,
@@ -12,7 +12,7 @@ import {
 } from "react-bootstrap";
 import "../App.css";
 
-function Registration() {
+function Login() {
   let navigate = useNavigate();
 
   function handleSubmit() {
@@ -43,6 +43,7 @@ function Registration() {
             <Image src={logo} />
           </div>
           <Form
+            onSubmit={handleSubmit}
             style={{ display: "inlineBlock" }}
             className="h-76 mt-2 pt-5 px-5"
           >
@@ -59,10 +60,10 @@ function Registration() {
               <Form.Control type="password" placeholder="Password" />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Confirm Password</Form.Label>
-              <Form.Control type="password" placeholder="Re-enter Password" />
-            </Form.Group>
+            <div>
+              If you don't have an account, click{" "}
+              <Link to="/registration">here</Link>
+            </div>
             <Stack
               gap="2"
               className="pt-4 mt-5 w-100 float-end align-bottom hstack gap-2"
@@ -73,12 +74,12 @@ function Registration() {
               </Button>
               <Button
                 className="ms-auto btn-color-orange"
-                onClick={handleSubmit}
                 variant="primary"
+                onClick={handleSubmit}
                 type="submit"
                 style={{ backgroundColor: "#FEB272" }}
               >
-                Register
+                Login
               </Button>
             </Stack>
           </Form>
@@ -88,4 +89,4 @@ function Registration() {
   );
 }
 
-export default Registration;
+export default Login;
