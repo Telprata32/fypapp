6d5833f0e09f275654530d34de66fe7bc43e5c50
Abdul Rahim <zero_degree_96@hotmail.com>
2022-07-01 17:02:00 +0800

    Fixed the Navbar for home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -17,33 +17,33 @@ import "../App.css";
 
 function Home() {
   return (
-    <Container className="px-0 mx-0 flex">
-      <Navbar style={{ backgroundColor: "#FFEFDD" }} expand="lg">
-        <Container>
-          <Navbar.Brand className="py-0">
-            <img src={logo} />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
+    <Container fluid className="px-0 mx-0">
+      <Navbar
+        className="px-4 py-1"
+        style={{ backgroundColor: "#FFEFDD" }}
+        expand="lg"
+      >
+        <Navbar.Brand className="py-0">
+          <img src={logo} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </Container>
   );
