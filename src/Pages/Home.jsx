c69bed6bc9dc49b0878df6c6e63e4686bfce9ed4
Abdul Rahim <zero_degree_96@hotmail.com>
2022-07-02 00:00:34 +0800

    Starting on Carousel

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,18 +1,17 @@
 import shopimg from "../Images/Shopping_edited.jpg";
 import logo from "../Images/appLogo.svg";
 import profpic from "../Images/istockphoto-1270067126-612x612.jpg";
+import prod1 from "../Images/Prod1.jpg";
+import prod2 from "../Images/Prod2.jpg";
+import prod3 from "../Images/Prod3.png";
 import { Link } from "react-router-dom";
 import {
   Navbar,
+  Row,
   Nav,
+  Carousel,
   NavDropdown,
   Container,
-  Row,
-  Button,
-  Col,
-  Form,
-  Image,
-  Stack,
 } from "react-bootstrap";
 import "../App.css";
 
@@ -20,7 +19,7 @@ function Home() {
   return (
     <Container fluid className="px-0 mx-0">
       <Navbar
-        className="px-4 py-1"
+        className="px-4 py-2"
         style={{ backgroundColor: "#FFEFDD" }}
         expand="lg"
       >
@@ -29,6 +28,36 @@ function Home() {
         </Navbar.Brand>
         <Nav style={{ backgroundImage: `url(${profpic})` }}></Nav>
       </Navbar>
+
+      <Row className="mt-5">
+        <Carousel>
+          <Carousel.Item>
+            <img className="d-block w-100" src={prod1} alt="First slide" />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={prod2} alt="Second slide" />
+
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={prod3} alt="Third slide" />
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </Row>
     </Container>
   );
 }
