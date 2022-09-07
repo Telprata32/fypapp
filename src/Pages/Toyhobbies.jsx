852421eba6487c99ee740740a7578ac5d82d050e
Abdul Rahim <zero_degree_96@hotmail.com>
2022-09-07 20:28:37 +0800

    Started on the Toys and Hobbies page

--- src/Pages/Toyhobbies.jsx
+++ src/Pages/Toyhobbies.jsx
@@ -1,5 +1,56 @@
+import { Row, Container, Card, Col } from "react-bootstrap";
+import "../App.css";
+
 function tHobbies() {
-  return <h1>This is the Toys and Hobbies page</h1>;
+  return (
+    <Container
+      className="tHobbies px-0"
+      style={{ marginTop: "157px", marginInline: "120px" }}
+    >
+      <Row>
+        <Col className="px-5" xl={3}>
+          <Card>
+            <Card.Img />
+            <Card.Body>
+              <Card.Title>Iphone X Pro</Card.Title>
+              <Card.Subtitle className="my-2">RM 1500.00</Card.Subtitle>
+              <Card.Text className="text-muted">IF Techshop</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col className="px-5" xl={3}>
+          <Card>
+            <Card.Img />
+            <Card.Body>
+              <Card.Title>Iphone X Pro</Card.Title>
+              <Card.Subtitle className="my-2">RM 1500.00</Card.Subtitle>
+              <Card.Text className="text-muted">IF Techshop</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col className="px-5" xl={3}>
+          <Card>
+            <Card.Img />
+            <Card.Body>
+              <Card.Title>Iphone X Pro</Card.Title>
+              <Card.Subtitle className="my-2">RM 1500.00</Card.Subtitle>
+              <Card.Text className="text-muted">IF Techshop</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col className="px-5" xl={3}>
+          <Card>
+            <Card.Img />
+            <Card.Body>
+              <Card.Title>Iphone X Pro</Card.Title>
+              <Card.Subtitle className="my-2">RM 1500.00</Card.Subtitle>
+              <Card.Text className="text-muted">IF Techshop</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default tHobbies;
