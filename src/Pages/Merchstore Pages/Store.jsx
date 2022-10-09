aba9d33e940f6e4ab751aaf146316b1f12dc5c70
Abdul Rahim <zero_degree_96@hotmail.com>
2022-10-09 15:07:30 +0800

    Refactored the codes for the store details section of  the MerchStore page

--- src/Pages/Merchstore Pages/Store.jsx
+++ src/Pages/Merchstore Pages/Store.jsx
@@ -21,6 +21,8 @@ function Store() {
   const [blcAcc, setAccount] = useState(""); // State for storing the account address of the blockchain account in the blockchain network
   const [storename, setName] = useState(""); //State for storing the user entered store name
   const [location, setLocation] = useState(""); //State for storing the user entered location of the store
+  const [thisStore, setStore] = useState({}); // Store this current account's Store details
+  const [thisAccount, setThisAccount] = useState({}); //Store current account's details
 
   // States inheritted from parent element via useOutletContext
   const [storeIsPressed, storePressed] = useOutletContext(); //state to keep track if the button at the bottom is pressed
@@ -36,7 +38,33 @@ function Store() {
     setAccCont(accContract);
     setAccount(accounts[0]);
 
-    // Load the account's store details here and store them into the states
+    // 1) get the count of how many user Accounts and Merchant stores exist in the smart contract
+    const aCount = await acontract.methods.acCount().call();
+    const mCount = await mcontract.methods.storecount().call();
+
+    // Load the account's account details here and store them into the states
+    for (let i = 1; i <= aCount; i++) {
+      const tempAcc = await acontract.methods.accounts(i).call();
+      if (tempAcc.email === cookies.Email) {
+        setThisAccount(tempAcc);
+        break;
+      }
+    }
+
+    // Load the account's merchant store details here and store them into the states
+    for (let i = 1; i <= aCount; i++) {
+      const tempStore = await mcontract.methods.Stores(i).call();
+      if (tempStore.account === cookies.Email) {
+        setStore(tempStore);
+        break;
+      }
+    }
+
+    //Fetch the details of this current account's store if the account has a merchant store
+    if (thisAccount.isMerchant) {
+      setName(thisStore.name);
+      setLocation(thisStore.location);
+    }
   };
 
   // Detect changes in the form and update the respective states
@@ -51,23 +79,9 @@ function Store() {
   // function to handle the form submit event
   const handlesubmit = async () => {
     // Check if the current account logged in has a merchant store or not (check if isMerchant is true)
-    // Declare a temporary variable to hold the isMerchant status of the account within this function
-    let isMerchant = false;
 
-    // 1) get the count of how many user Accounts exist in the smart contract
-    const aCount = await acontract.methods.acCount().call();
-
-    // 2) check through every record in the blockchain smart contract to find the account and retrieve isMerchant status
-    for (let i = 1; i <= aCount; i++) {
-      const tempAcc = await acontract.methods.accounts(i).call();
-      if (tempAcc.email === cookies.Email) {
-        isMerchant = tempAcc.isMerchant;
-        break;
-      }
-    }
-
-    // Now if the isMerchant is false then create a new store record in the blockchain, if not just update it
-    if (isMerchant) {
+    // If the isMerchant is false then create a new store record in the blockchain, if not just update it
+    if (thisAccount.isMerchant) {
       // create a new store record in the blockchain for the account
       mcontract.methods
         .createStore(cookies.Email, storename, location)
